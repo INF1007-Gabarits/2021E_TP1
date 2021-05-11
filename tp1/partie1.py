4a4f740f9c004c058bd1c20725bcf9636762e2e1
Rose Hirigoyen <rosehirigoyen@gmail.com>
2021-05-11 15:48:05 -0400

    tp1

---
+++ tp1/partie1.py
@@ -0,0 +1,32 @@
+# PARTIE 1: Fonctions de base de la calculatrice
+
+def additionner(a, b):
+    # TODO: calculer somme de a et b
+    somme =
+
+    return somme
+
+def soustraire(a, b):
+    # TODO: calculer différence entre a et b
+    difference =
+
+    return difference
+
+def multiplier(a, b):
+    # TODO: calculer produit entre a et b
+    produit =
+
+    return produit
+
+def diviserAvecReste(a, b):
+    # TODO: calculer quotient (ex: 5 / 2 = 2.5)
+    quotient =
+    # TODO: calculer reste ( ex: 5 / 2 reste 1)
+    reste =
+
+    return quotient, reste
+
+
+# PAS TOUCHER
+if __name__ == "__main__":
+    print("Exécutez le fichier calculatrice.py ou le fichier tests.py!")
